dc44243bec5a9985be531786490ff12a1a13b37c
erroldepa <errold.epa@gmail.com>
2025-10-22 01:39:13 +0800

    ad URL's and SECRET

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -5,12 +5,13 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      jwtSecret: process.env.JWT_SECRET || "sEGH8gzdQfQ5u90Nn8RLoDmQ+Z8hGtHzXtOwcQOXYZKHdnYMuYN7z1vx3gVEXl6Q",
+      cookieSecret: process.env.COOKIE_SECRET || "CrzBLEVRcQIg3wBxffo6Z/jN29NHMd4KxtVosMZIEZ0E470gk2wkNyenvyhqVm3q",
     }
   }
 })
